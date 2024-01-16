5ab79fc8912452d5487829122915802f634cd236
Jangeunhye <dmsgp220@gmail.com>
2024-01-17 01:38:13 +0900

    리액트, 뷰 기초 기능

---
+++ vue/src/components/NewTodo.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header">
+    <input
+      class="new-todo"
+      autofocus
+      autocomplete="off"
+      placeholder="Todo"
+      v-model="text"
+      @keypress="handleTodo"
+    />
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    handleTodo({ keyCode }) {
+      const { text } = this;
+
+      if (keyCode === 13 && text !== "") {
+        this.$emit("insertTodo", text);
+        this.text = "";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+header {
+  margin: 0 auto;
+}
+.new-todo {
+  border: none;
+  width: 33rem;
+  border-bottom: 2px solid;
+  padding-left: 1rem;
+}
+</style>
